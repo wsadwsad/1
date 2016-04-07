61af0bf7d5fee1815fecd2b6d49fccd0d8d5f7e0
Raul Zuniga <rzuniga64@gmail.com>
2016-04-07 01:25:58 -0500

    Mofied code for second player. Game sorta works now.

--- public/lib/game/entities/player.js
+++ public/lib/game/entities/player.js
@@ -156,26 +156,28 @@ ig.module(
 		EntityOtherplayer = ig.Entity.extend({
             name: "otherplayer",
             gamename: "",
+            animation: 0,
 			size: {x: 32, y: 40},
 			maxVel: {x: 400, y: 800},
 
-			type: ig.Entity.TYPE.B, // Player friendly group
+			type: ig.Entity.TYPE.A, // Player friendly group
 			checkAgainst: ig.Entity.TYPE.NONE,
 			collides: ig.Entity.COLLIDES.PASSIVE,
-
-			animSheet: new ig.AnimationSheet( 'media/enemy1.png', 30, 25 ),
-
-			sfxHurt: new ig.Sound( 'media/sounds/hurt2.*' ),
-			sfxreload: new ig.Sound( 'media/sounds/reload.*' ),
-			sfxDie: new ig.Sound( 'media/sounds/Blood_Squirt.*' ),
 
 			health: 3,
 			flip: false,
 			move: 200,
 			jump: 450,
 			maxHealth: 3,
-
 			coins: 0,
+
+            animSheet: new ig.AnimationSheet( 'media/player.png', 38, 41 ),
+
+            sfxHurt: new ig.Sound( 'media/sounds/hurt2.*' ),
+            sfxreload: new ig.Sound( 'media/sounds/reload.*' ),
+            sfxDie: new ig.Sound( 'media/sounds/Blood_Squirt.*' ),
+
+
 
 			init: function( x, y, settings ) {
 				this.parent( x, y, settings );
@@ -188,105 +190,28 @@ ig.module(
 
 				ig.game.player = this;
 			},
-
+            // place the new player
+            netmoveplayer: function() {
+                this.pos.x = positionx;
+                this.pos.y = positiony;
+            },
 			update: function() {
-				var move = this.move;
-				if( ig.input.state('left') ) {
-					this.vel.x = -move;
-					this.flip = true;
-				}
-				else if( ig.input.state('right') ) {
-					this.vel.x = move;
-					this.flip = false;
-				}
-				else {
-					this.vel.x = 0;
-				}
+                switch (this.animation) {
+                    case 1:
+                        this.currentAnim = this.anims.jump;
+                        break;
+                    case 2:
+                        this.currentAnim = this.anims.down;
+                        break;
+                    case 3:
+                        this.currentAnim = this.anims.fall;
+                        break;
+                    case 4:
+                        this.currentAnim = this.anims.idle;
+                        break;
 
-				if( this.standing && ig.input.pressed('jump') ) {
-					this.vel.y = -this.jump;
-				}
-
-				if( ig.input.pressed('shoot') ) {
-					if (this.coins < 2){
-						ig.game.spawnEntity( EntityFireball, this.pos.x+10, this.pos.y+10, {flip:this.flip} );
-					}
-					else if (this.coins >= 5){
-						ig.game.spawnEntity( EntityFireball3, this.pos.x+5, this.pos.y+10, {flip:this.flip} );
-					}
-					else if (this.coins >= 2){
-						ig.game.spawnEntity( EntityFireball2, this.pos.x+10, this.pos.y+10, {flip:this.flip} );
-					}
-				}
-
-				if(this.currentAnim == this.anims.pain &&
-					this.currentAnim.loopCount < 1
-				) {
-					if( this.health <= 0 ) {
-						var dec = (1/this.currentAnim.frameTime) * ig.system.tick;
-						this.currentAnim.alpha = (this.currentAnim.alpha - dec).limit(0,1);
-					}
-				}
-				else if( this.health <= 0 ) {
-					this.kill();
-				}
-				else if( this.vel.y < 0 ) {
-					this.currentAnim = this.anims.jump;
-				}
-				else if( this.vel.y > 0 ) {
-					if( this.currentAnim != this.anims.fall ) {
-						this.currentAnim = this.anims.fall.rewind();
-					}
-				}
-				else if( this.vel.x != 0 ) {
-					this.currentAnim = this.anims.run;
-				}
-				else {
-					this.currentAnim = this.anims.idle;
-				}
-
-				this.currentAnim.flip.x = this.flip;
-
-				this.parent();
-			},
-
-			kill: function() {
-
-				this.sfxDie.play();
-
-				this.parent();
-
-				ig.game.reloadLevel();
-
-			},
-
-			giveCoins: function( amount ) {
-				this.coins += amount;
-				if (this.coins == 2)
-				{
-					this.sfxreload.play();
-				}
-				else if (this.coins == 5)
-				{
-					this.sfxreload.play();
-				}
-			},
-
-
-			receiveDamage: function( amount, from ) {
-				if( this.currentAnim == this.anims.pain ) {
-					return;
-				}
-				this.health -= amount;
-				if(this.coins > 1)
-				{
-					this.coins -= 2;
-				}
-				this.currentAnim = this.anims.pain.rewind();
-
-				this.vel.x = (from.pos.x > this.pos.x) ? -400 : 400;
-				this.vel.y = -300;
-				this.sfxHurt.play();
-			}
+                }
+                this.parent();
+            }
 		});
 	});
